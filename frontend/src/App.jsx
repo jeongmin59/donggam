69a7756d4e14a379b5508b045b764a98c5d481e1
양불회 <yubeak01@naver.com>
2023-10-24 10:40:35 +0900

    :sparkles: Feat: recoilroot 위치 변경

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import {
-  RecoilRoot,
-  atom,
-  selector,
-  useRecoilState,
-  useRecoilValue,
-} from "recoil";
 import "./App.css";
 import MainPage from "./pages/MainPage";
 
 function App() {
   return (
-    <RecoilRoot>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-        </Routes>
-      </BrowserRouter>
-    </RecoilRoot>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 

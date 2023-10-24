f1867e9282190a9d2853d26b1d2f99c68fcfb786
공정민 <jmgong59@gmail.com>
2023-10-24 17:46:48 +0900

    :sparkles: Feat : 시간 페이지 추가

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import "./index.css";
 import MainPage from "./pages/MainPage";
 import LoginPage from "./pages/LoginPage";
 import MailboxPage from "./pages/MailboxPage";
+import TimePage from "./pages/TimePage"
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<MainPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/mailbox/:userId" element={<MailboxPage />} />
+        <Route path="/time" element={<TimePage />} />
       </Routes>
     </BrowserRouter>
   );

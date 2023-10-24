b9f9e0a6a776c5491510779c99ea81ff27dde465
양불회 <yubeak01@naver.com>
2023-10-24 15:27:36 +0900

    :sparkles: Feat: 쪽지함 페이지 생성

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import MainPage from "./pages/MainPage";
 import LoginPage from "./pages/LoginPage";
+import MailboxPage from "./pages/MailboxPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<LoginPage />} />
         <Route path="/" element={<MainPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/mailbox/:userId" element={<MailboxPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/pages/MailboxPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MailboxPage = () => {
+  return (
+    <div>
+      쪽지함 페이지
+    </div>
+  );
+};
+
+export default MailboxPage;

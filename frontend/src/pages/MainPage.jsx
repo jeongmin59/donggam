23e0ba1866971c15057072ebe422413ea041e1c1
박진희 <wlsl0620@naver.com>
2023-10-24 12:16:50 +0900

    💄 Design : 메인페이지 배경 이미지 삽입

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
 
 const MainPage = () => {
-  return <div className=" bg-sky-500">메인페이지</div>;
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
+
+  useEffect(()=>{
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position)=> {
+        setLatitude(position.coords.latitude);
+        setLongitude(position.coords.longitude);
+      }, (e) => {
+        console.log(e.meesage);
+      });
+    } else {
+      console.log('위치 정보를 지원하지 않는 브라우저입니다.')
+    }
+  },[])
+
+  return (
+    // <div className=" bg-sky-500">메인페이지</div>
+    <div className="w-full h-screen relative overflow-hidden bg-[#abcdf0]">
+      <div>
+        메인페이지
+        <p>위도 : {latitude}</p>
+        <p>경도 : {longitude}</p>        
+      </div>
+      <img src="/background-image.png"
+        className="w-full h-full absolute top-40 object-cover opacity-60 " />
+    </div>
+  );
 };
 
 export default MainPage;

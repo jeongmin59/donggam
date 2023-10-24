7aaa4bd8139871ba753c1f5e33f0cf8b223592c7
박진희 <wlsl0620@naver.com>
2023-10-24 17:27:14 +0900

    💄 Design : 메인페이지 날씨별 배경 설정

--- frontend/src/pages/MainPage.jsx
+++ frontend/src/pages/MainPage.jsx
@@ -1,34 +1,11 @@
-import React, { useEffect } from "react";
-import { useState } from "react";
+import React from "react";
+import MainBackground from "../components/mainpage/MainBackground";
 
 const MainPage = () => {
-  const [latitude, setLatitude] = useState(null);
-  const [longitude, setLongitude] = useState(null);
-
-  useEffect(()=>{
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position)=> {
-        setLatitude(position.coords.latitude);
-        setLongitude(position.coords.longitude);
-      }, (e) => {
-        console.log(e.meesage);
-      });
-    } else {
-      console.log('위치 정보를 지원하지 않는 브라우저입니다.')
-    }
-  },[])
-
   return (
-    // <div className=" bg-sky-500">메인페이지</div>
-    <div className="w-full h-screen relative overflow-hidden bg-[#abcdf0]">
-      <div>
-        메인페이지
-        <p>위도 : {latitude}</p>
-        <p>경도 : {longitude}</p>        
-      </div>
-      <img src="/background-image.png"
-        className="w-full h-full absolute top-40 object-cover opacity-60 " />
-    </div>
+    <>
+      <MainBackground /> 
+    </>
   );
 };
 
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage:{
+        'gradient-1': 'linear-gradient(180deg, rgba(121, 187, 254, 0.70) 0%, rgba(255, 255, 255, 0.70) 17.6%, rgba(255, 200, 183, 0.70) 52.9%, rgba(162, 208, 255, 0.70) 83.23%)',
+        'gradient-2': 'linear-gradient(180deg, rgba(102, 177, 255, 0.70) 3.3%, rgba(255, 255, 255, 0.70) 17.02%, rgba(188, 220, 254, 0.70) 53.12%, rgba(102, 177, 255, 0.70) 100%)',
+        'gradient-3': 'linear-gradient(180deg, rgba(129, 155, 181, 0.70) 3.3%, rgba(255, 255, 255, 0.70) 18.65%, rgba(169, 189, 209, 0.70) 53.02%)',
+      }
+    },
   },
   plugins: [],
 }

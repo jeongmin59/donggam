baf637a224a64a616db239b99d83248f2878d94f
박진희 <wlsl0620@naver.com>
2023-10-25 11:00:20 +0900

    💄 Design : 유저 정보 컨테이너 설정
    Change-Id: Iacce57f09e6f6a5b34d32f9cba61fcfbebedc1af

--- frontend/src/components/mainpage/UserInfo.jsx
+++ frontend/src/components/mainpage/UserInfo.jsx
@@ -1,29 +1,36 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 
 const UserInfo = () => {
-  const [latitude, setLatitude] = useState(null);
-  const [longitude, setLongitude] = useState(null);
+  // const [latitude, setLatitude] = useState(null);
+  // const [longitude, setLongitude] = useState(null);
 
-  // 현위치
-  useEffect(()=>{
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position)=> {
-        setLatitude(position.coords.latitude);
-        setLongitude(position.coords.longitude);
-      }, (e) => {
-        console.log(e.meesage);
-      });
-    } else {
-      console.log('위치 정보를 지원하지 않는 브라우저입니다.')
-    }
-  },[])
+  // // 현위치
+  // useEffect(()=>{
+  //   if (navigator.geolocation) {
+  //     navigator.geolocation.getCurrentPosition((position)=> {
+  //       setLatitude(position.coords.latitude);
+  //       setLongitude(position.coords.longitude);
+  //     }, (e) => {
+  //       console.log(e.meesage);
+  //     });
+  //   } else {
+  //     console.log('위치 정보를 지원하지 않는 브라우저입니다.')
+  //   }
+  // },[])
 
   return(
-    <div className="bg-white w-80">
-      <p>위도 : {latitude}</p>
-      <p>경도 : {longitude}</p>        
-      <p>사용자이름</p>
-      <p>사용자 상태메시지</p>
+    <div className="w-80 h-[70px] mx-auto mt-[78px]">
+      <p className="username mb-[10px]">사용자 이름</p>
+      <div className="usermsg h-full bg-white rounded-2xl flex justify-center">
+        <p>상태메시지를 입력하세요</p>
+      </div>
+
+      {/* <p>위도 : {latitude}</p>
+      <p>경도 : {longitude}</p>         */}
+      {/* <p>사용자이름</p>
+      <div className="bg-white w-80">
+        <p>사용자 상태메시지</p>
+      </div> */}
     </div>
   );
 };

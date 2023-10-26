74565f41f1d0444fbfea98e4d10a0294c4a0bde3
양불회 <yubeak01@naver.com>
2023-10-26 17:26:31 +0900

    :sparkles: Feat: user관련 api 인스턴스 생성
    Change-Id: I0e0de34da29322c12d475d62d89da510c9abf5ea

---
+++ frontend/src/api/userApi.jsx
@@ -0,0 +1,46 @@
+import axiosInstance from "./axiosConfig";
+
+// 닉네임 변경
+export const updateNickname = async (nickname) => {
+  try {
+    const res = await axiosInstance.put(`/member/nickname`, {
+      params: {
+        nickname: nickname
+      }
+    });
+    return res.data;
+  } catch (err) {
+    console.log('닉네임 변경 실패!', err)
+    return err;
+  }
+};
+
+// 상태메시지 변경
+export const updateStatus = async (status) => {
+  try {
+    const res = await axiosInstance.put(`/member/status`, {
+      params: {
+        status: status
+      }
+    });
+    return res.data;
+  } catch (err) {
+    console.log('상태메시지 변경 실패!', err)
+    return err;
+  }
+};
+
+// 캐릭터 변경
+export const updateCharacter = async (characterId) => {
+  try {
+    const res = await axiosInstance.put(`/member/character`, {
+      params: {
+        characterId: characterId
+      }
+    });
+    return res.data;
+  } catch (err) {
+    console.log('캐릭터 변경 실패!', err)
+    return err;
+  }
+};

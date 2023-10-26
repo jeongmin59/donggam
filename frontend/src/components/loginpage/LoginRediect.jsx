fc6f1f50fe9d10331f5e50c824891cb05270c863
양불회 <yubeak01@naver.com>
2023-10-26 16:56:45 +0900

    :sparkles: Feat: userAtom 생성
    Change-Id: I6e5baf4487a14baf10b7183970ea6a6cb882f89c

--- frontend/src/components/loginpage/LoginRediect.jsx
+++ frontend/src/components/loginpage/LoginRediect.jsx
@@ -1,10 +1,14 @@
 import axios from 'axios';
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { MemberIdAtom } from '../../recoil/user/userAtom';
+import { useRecoilState } from 'recoil';
 
 
 const LoginRediect = () => {
   const navigator = useNavigate();
+
+  const [memberId, setMemberId] = useRecoilState(MemberIdAtom)
 
   // REDIRECT_URL의 Params로 온 인가코드 받아오기
   const code = new URL(window.location.href).searchParams.get('code');
@@ -15,6 +19,11 @@ const LoginRediect = () => {
       .then((res) => {
         console.log('성공!!', res.data);
         window.localStorage.setItem("accessToken", res.data.data.accessToken); //localStorage에 accessToken 저장하기
+
+        //userAtom 업데이트
+        setMemberId(res.data.data.memberId)
+        console.log(res.data.data.memberId)
+
         navigator('/') // 로그인 성공 후 메인페이지로 이동
       })
       .catch(err => {
---
+++ frontend/src/recoil/user/userAtom.jsx
@@ -0,0 +1,42 @@
+import { atom } from 'recoil';
+import { recoilPersist } from 'recoil-persist';
+
+const { persistAtom } = recoilPersist({
+  key: "User",
+  storage: localStorage,
+})
+
+//액세스 토큰
+export const AccessToken = atom({
+  key: 'AccessToken',
+  default: null,
+  effects_UNSTABLE: [persistAtom]
+})
+
+//유저 아이디
+export const MemberIdAtom = atom({
+  key: 'MemberIdAtom',
+  default: null,
+  effects_UNSTABLE: [persistAtom]
+})
+
+// 닉네임
+export const NicknameAtom = atom({
+  key: 'NicknameAtom',
+  default: '',
+  effects_UNSTABLE: [persistAtom]
+});
+
+// 캐릭터 id
+export const CharacterIdAtom = atom({
+  key: 'CharacterIdAtom',
+  default: null,
+  effects_UNSTABLE: [persistAtom]
+});
+
+// 상태 메시지
+export const StatusMessageAtom = atom({
+  key: 'StatusMessageAtom',
+  default: '',
+  effects_UNSTABLE: [persistAtom]
+});

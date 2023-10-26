b49282905711f97d80940932034a69fecd000b6e
최동우 <fdf5548@naver.com>
2023-10-26 09:08:21 +0900

    ✨ Feat : Cors - 허용 Origin 추가

--- backend/src/main/java/com/example/backend/config/CorsConfig.java
+++ backend/src/main/java/com/example/backend/config/CorsConfig.java
@@ -1,18 +1,18 @@
 package com.example.backend.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 
-public class WebConfig implements WebMvcConfigurer {
+public class CorsConfig implements WebMvcConfigurer {
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-        .allowedOrigins("http://localhost:3000", "http://localhost:3001")
+        .allowedOrigins("http://localhost:3000", "http://localhost:5173", "http://k9e107a.p.ssafy.io",
+            "https://k9e107a.p.ssafy.io", "http://ditto.site", "https://ditto.site")
         .allowedMethods("GET", "POST", "PUT", "DELETE")
         .allowedHeaders("*")
         .maxAge(3600);

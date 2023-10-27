300e0fd65cd4084e9f312d77acafdbd2fc4fa268
최동우 <fdf5548@naver.com>
2023-10-27 14:32:22 +0900

    ✨ Feat : 이미지 전체 조회
    Change-Id: I41bbfbfe60f5562ae56ef9643d909dc60be12ad2

--- backend/src/main/java/com/example/backend/repository/mariaDB/ImageRepository.java
+++ backend/src/main/java/com/example/backend/repository/mariaDB/ImageRepository.java
@@ -1,8 +1,12 @@
 package com.example.backend.repository.mariaDB;
 
 import com.example.backend.entity.mariaDB.time.Image;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ImageRepository extends JpaRepository<Image, Long> {
 
+  @Query("SELECT DISTINCT i FROM Image i LEFT JOIN FETCH i.author LEFT JOIN FETCH i.likeMember")
+  List<Image> findAllWithAuthorAndLikeMember();
 }

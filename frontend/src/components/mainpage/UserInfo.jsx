beaa6a37ccb5f8718a2c503ffda7988c7e62635e
박진희 <wlsl0620@naver.com>
2023-10-31 00:45:41 +0900

    💄 Design : 날씨 구름 이미지 추가
    Change-Id: I3d4e094ea1c183774b218f6c7bc6a7b855c9fa31

--- frontend/src/components/mainpage/UserInfo.jsx
+++ frontend/src/components/mainpage/UserInfo.jsx
@@ -1,9 +1,11 @@
 // import { useState, useEffect } from "react";
-
 import { useRecoilValue } from "recoil";
 import { UserSelector } from "../../recoil/user/userSelector";
+import PositiveImg from '../../assets/images/positive.svg';
+import NeutralImg from '../../assets/images/neutral.svg';
+import NegativeImg from '../../assets/images/negative.svg';
 
-const UserInfo = () => {
+const UserInfo = ({selectedBackground}) => {
   // 전역 user 정보 가져오기
   const user = useRecoilValue(UserSelector);
   const nickname = user.nickname
@@ -13,6 +15,7 @@ const UserInfo = () => {
   return (
     <div className="userInfo h-100 px-[24px] mx-auto mt-[60px]">
       <h2 className="username mb-[10px] pl-3">{nickname}</h2>
+      {/* <img src={PositiveImg}/> */}
       <div className="usermsg h-auto bg-white rounded-2xl flex justify-center">
         <p className="px-[40px] py-[20px] text-center ">{statusMessage}</p>
       </div>

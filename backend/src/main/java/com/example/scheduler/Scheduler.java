f8fa7eab02268a6cac94a075cd62d556902d15b4
최동우 <fdf5548@naver.com>
2023-10-30 16:07:45 +0900

    ✨ Feat : 스프링 스케줄러 설정
    Change-Id: I25588e2264cd17aad6e20854b9ccfe7d56b8170d

---
+++ backend/src/main/java/com/example/scheduler/Scheduler.java
@@ -0,0 +1,12 @@
+package com.example.scheduler;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class Scheduler {
+  
+  // 일정 주기마다 실행될 로직 작성
+
+}

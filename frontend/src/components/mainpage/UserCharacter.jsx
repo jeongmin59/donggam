b985294e2529b72c4e7c66ca60fcadde0936b0ff
박진희 <wlsl0620@naver.com>
2023-10-31 17:32:03 +0900

    :sparkles: Feat : UserCharacter 컴포넌트 분리
    Change-Id: I2190c7d597cd9365327065fa98fefd717c4dba9a

---
+++ frontend/src/components/mainpage/UserCharacter.jsx
@@ -0,0 +1,40 @@
+const UserCharacter = ({ character, existingCharacters }) => {
+  // 랜덤 위치 배정
+  let positionX, positionY;
+  let isOverlapping;
+
+  do {
+    isOverlapping = false;
+    positionX = Math.floor(Math.random() * (70 - 20) + 20);
+    positionY = Math.floor(Math.random() * (75 - 15) + 15);
+
+    for (const existingCharacter of existingCharacters) {
+      const charX = existingCharacter.left;
+      const charY = existingCharacter.top;
+      const dx = Math.abs(positionX - parseInt(charX));
+      const dy = Math.abs(positionY - parseInt(charY));
+
+      if (dx < 200 && dy < 200) {
+        isOverlapping = true;
+        break;
+      }
+    }
+  } while (isOverlapping);
+
+  return (
+    <div
+      className="user-element"
+      style={{
+        position: "absolute",
+        left: positionX + "%",
+        top: positionY + "%",
+        width: "30%", // 이미지 크기에 따라 설정 == > 반응형 하니까 너무 커짐 
+        padding: "10px",
+      }}
+    >
+      <img src={`/character/${character}.svg`} style={{ width: "100%" }} />
+    </div>
+  );
+};
+
+export default UserCharacter;
--- frontend/src/components/mainpage/UserLocation.jsx
+++ frontend/src/components/mainpage/UserLocation.jsx
@@ -1,54 +1,48 @@
 import { useRecoilValue } from "recoil";
 import { UserSelector } from "../../recoil/user/userSelector";
-import React from "react";
 import Lottie from "react-lottie";
 import animationData from "../../assets/animation/location-animation.json";
+import UserCharacter from "./UserCharacter";
 
-const getRandomPosition = (min, max, exceptions = []) => {
-  let position;
-  do {
-    position = Math.floor(Math.random() * (max - min) + min) + "%";
-  } while (exceptions.includes(position));
-  return position;
-};
 
-const UserCharacter = ({ character, existingCharacters }) => {
-  let positionX, positionY;
-  let isOverlapping;
+// const UserCharacter = ({ character, existingCharacters }) => {
+//   // 랜덤 위치 배정
+//   let positionX, positionY;
+//   let isOverlapping;
 
-  do {
-    isOverlapping = false;
-    positionX = Math.floor(Math.random() * (70 - 20) + 20);
-    positionY = Math.floor(Math.random() * (75 - 15) + 15);
+//   do {
+//     isOverlapping = false;
+//     positionX = Math.floor(Math.random() * (70 - 20) + 20);
+//     positionY = Math.floor(Math.random() * (75 - 15) + 15);
 
-    for (const existingCharacter of existingCharacters) {
-      const charX = existingCharacter.left;
-      const charY = existingCharacter.top;
-      const dx = Math.abs(positionX - parseInt(charX));
-      const dy = Math.abs(positionY - parseInt(charY));
+//     for (const existingCharacter of existingCharacters) {
+//       const charX = existingCharacter.left;
+//       const charY = existingCharacter.top;
+//       const dx = Math.abs(positionX - parseInt(charX));
+//       const dy = Math.abs(positionY - parseInt(charY));
 
-      if (dx < 200 && dy < 200) {
-        isOverlapping = true;
-        break;
-      }
-    }
-  } while (isOverlapping);
+//       if (dx < 200 && dy < 200) {
+//         isOverlapping = true;
+//         break;
+//       }
+//     }
+//   } while (isOverlapping);
 
-  return (
-    <div
-      className="user-element"
-      style={{
-        position: "absolute",
-        left: positionX + "%",
-        top: positionY + "%",
-        width: "30%", // 이미지 크기에 따라 설정
-        padding: "10px",
-      }}
-    >
-      <img src={`/character/${character}.svg`} style={{ width: "100%" }} />
-    </div>
-  );
-};
+//   return (
+//     <div
+//       className="user-element"
+//       style={{
+//         position: "absolute",
+//         left: positionX + "%",
+//         top: positionY + "%",
+//         width: "30%", // 이미지 크기에 따라 설정
+//         padding: "10px",
+//       }}
+//     >
+//       <img src={`/character/${character}.svg`} style={{ width: "100%" }} />
+//     </div>
+//   );
+// };
 
 const UserLocation = ({ userCharacters }) => {
   const user = useRecoilValue(UserSelector);
@@ -68,6 +62,8 @@ const UserLocation = ({ userCharacters }) => {
     transform: "scale(2)", // 2배 크기로 확대
   };
 
+
+
   return (
     <div className="flex justify-center items-center overflow-hidden" style={{ width: "100%", height: "100%", zIndex: -1 }}>
       <div className="nya relative flex justify-center items-center" style={{ width: "100%", height: "100%", zIndex: 1 }}>
@@ -75,7 +71,10 @@ const UserLocation = ({ userCharacters }) => {
           <img src={myCharacter} alt={`${characterId}번 캐릭터`} />
         </div>
         {userCharacters.map((character, index) => (
-          <UserCharacter key={index} character={character} existingCharacters={userCharacters} />
+          <UserCharacter 
+            key={index} 
+            character={character} 
+            existingCharacters={userCharacters} />
         ))}
       </div>
       <div className="flex absolute" style={{ height: "calc(100% - 280px)", zIndex: -1 }}>

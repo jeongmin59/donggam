516e512aabff8c82fa0b0ddaaebef8bba41dc8c7
양불회 <yubeak01@naver.com>
2023-11-01 09:20:26 +0900

    :memo: Feat: 쪽지함페이지 라우팅 주소 변경 및 컴포넌트 구조 설계
    Change-Id: I9dd8090dd3175eeac3bba27560c8dfdfbd66b052

---
+++ frontend/src/components/mailboxpage/MailBox.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MailList from './MailList';
+
+const MailBox = () => {
+  return (
+    <div>
+      <h1>메일 박스임 </h1>
+      <MailList />
+    </div>
+  );
+};
+
+export default MailBox;

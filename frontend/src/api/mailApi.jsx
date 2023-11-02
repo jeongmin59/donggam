3146d766b381f4ca38e51a576bfebb085ffe49cb
박진희 <wlsl0620@naver.com>
2023-11-02 14:47:14 +0900

    :sparkles: Feat : 쪽지 전송
    Change-Id: Ia32f687509486f2fa85f1454151fc4a4bb59c19e

--- frontend/src/api/mailApi.jsx
+++ frontend/src/api/mailApi.jsx
@@ -1,5 +1,29 @@
 import axiosInstance from "./axiosConfig";
 
+// 쪽지 전송
+export const sendMail =async (statusId, content, imageFile) => {
+  try {
+    const message = {
+      "statusId" : statusId,
+      "content" : content,
+    }
+    const formData = new FormData();
+    formData.append('request', new Blob([JSON.stringify(message)], { type: 'application/json' }));
+    formData.append('image', imageFile);
+    
+    const res = await axiosInstance.post(`/message/send`, formData,{
+      headers: {
+        'Content-Type': 'multipart/form-data',
+        // "Content-Type": "application/json",
+      },
+    });
+    console.log('쪽지 전송 axios 성공', res.data);
+    return res.data;
+  } catch (err) {
+    console.log('쪽지 전송 axios 실패', err)
+  }
+}
+
 // 전체 status 리스트 가져오기
 export const getStatusList = async () => {
   try {

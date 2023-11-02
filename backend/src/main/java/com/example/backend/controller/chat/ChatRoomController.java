bc0df23f2fafaa5e642710ed3af65c9a7fa2c47c
junghyunwoo <wjdgusdndi@gmail.com>
2023-11-02 11:24:43 +0900

    :sparkles: Feat : 채팅 신청 기능 및 메세지 조회시 날짜 추가

--- backend/src/main/java/com/example/backend/controller/chat/ChatRoomController.java
+++ backend/src/main/java/com/example/backend/controller/chat/ChatRoomController.java
@@ -3,6 +3,7 @@ package com.example.backend.controller.chat;
 import com.example.backend.dto.Response;
 import com.example.backend.dto.chat.GetChatListDto;
 import com.example.backend.dto.chat.GetRoomListDto;
+import com.example.backend.dto.chat.InviteChatDto;
 import com.example.backend.service.chat.ChatRoomService;
 import com.example.backend.service.chat.ChatService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -12,6 +13,8 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "채팅방 API")
@@ -24,9 +27,10 @@ public class ChatRoomController {
 
     @Operation(summary = "채팅방 목록 조회 API", description = "채팅방 목록 조회 API")
     @GetMapping("/room/list")
-    public Response<GetRoomListDto.Response> getRoomList(@AuthenticationPrincipal UserDetails userDetails) {
+    public Response<GetRoomListDto.Response> getRoomList(
+            @AuthenticationPrincipal UserDetails userDetails) {
         Long memberId = Long.parseLong(userDetails.getUsername());
-        GetRoomListDto.Response response =chatRoomService.getRoomList(memberId);
+        GetRoomListDto.Response response = chatRoomService.getRoomList(memberId);
         return new Response<>(200, "채팅방 조회 완료", response);
     }
 
@@ -36,5 +40,14 @@ public class ChatRoomController {
     public Response<GetChatListDto.Response> getChatList(@PathVariable Long roomId) {
         GetChatListDto.Response response = chatService.getChatList(roomId);
         return new Response<>(200, "채팅 내용 조회 완료", response);
+    }
+
+    @Operation(summary = "채팅 신청", description = "채팅 신청")
+    @PostMapping("/chat/invite")
+    public Response<> inviteChat(@RequestBody InviteChatDto.Request request,
+            @AuthenticationPrincipal UserDetails userDetails) {
+        Long memberId = Long.parseLong(userDetails.getUsername());
+        chatService.inviteChat(request, memberId);
+        return new Response<>(201, "채팅 신청 완료");
     }
 }

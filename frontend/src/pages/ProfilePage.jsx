4f10c7bd260dc13d7b607caf39d254b5242b88a9
공정민 <jmgong59@gmail.com>
2023-11-02 21:41:30 +0900

    :lipstick: Design : 프로필 화면 전체 css 수정 완료

--- frontend/src/pages/ProfilePage.jsx
+++ frontend/src/pages/ProfilePage.jsx
@@ -1,13 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useRecoilState, useSetRecoilState } from 'recoil';
 import { CharacterIdAtom, NicknameAtom, StatusMessageAtom, StatusMessageIdAtom } from '../recoil/user/userAtom';
 import { updateUser } from '../api/userApi';
 import { useNavigate } from 'react-router-dom';
 import CharacterModal from '../components/character/CharacterModal';
-import Header from '../components/common/Header';
+import ProfileHeader from '../components/character/ProfileHeader';
+import editIcon from '../assets/icons/edit-icon.png';
 
 const ProfilePage = () => {
-  // 전역상태의 유저 정보 가져오기
+  // 전역 상태의 유저 정보 가져오기
   const [nickName, setNickname] = useRecoilState(NicknameAtom);
   const [characterId, setCharterId] = useRecoilState(CharacterIdAtom);
   const [status, setStatus] = useRecoilState(StatusMessageAtom);
@@ -18,40 +19,40 @@ const ProfilePage = () => {
   const [newCharacterId, setNewCharacterId] = useState(characterId);
   const [newStatus, setNewStatus] = useState(status);
 
-  console.log(newCharacterId)
-
-  // 캐릭터 이미지 주소
-  const myCharacter = `/character/${characterId}.svg`
-
   // 유저 정보 업데이트 후 네비게이트 해주기
   const navigator = useNavigate();
 
   // 모달 열고 닫기
-  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
+  // newCharacterId가 변경될 때 렌더링
+  useEffect(() => {
+    // newCharacterId가 변경될 때 실행
+    const myCharacter = `/character/${newCharacterId}.svg`;
+  }, [newCharacterId]);
+
+  // 캐릭터 이미지 주소
+  const myCharacter = `/character/${newCharacterId}.svg`;
 
   const handleUserUpdateClick = () => {
-
     const updatedUser = {
       nickname: newNickname,
       status: newStatus,
       characterId: newCharacterId,
-    }
+    };
     updateUser(updatedUser)
-
       .then((res) => {
-        setNickname(newNickname)
-        setCharterId(newCharacterId)
-        setStatus(newStatus)
-        setStatusId(res.data.statusId)
-
-        navigator('/') // 정보 수정 후 메인페이지 이동
-        console.log('유저정보 변경', res)
+        setNickname(newNickname);
+        setCharterId(newCharacterId);
+        setStatus(newStatus);
+        setStatusId(res.data.statusId);
+        navigator('/'); // 정보 수정 후 메인페이지 이동
+        console.log('유저정보 변경', res);
       })
       .catch((err) => {
-        console.log('유저 정보 변경 안됨 ㄱ-', err)
-      })
-  }
+        console.log('유저 정보 변경 안됨 ㄱ-', err);
+      });
+  };
 
   const handleNicknameChange = (e) => {
     setNewNickname(e.target.value);
@@ -67,56 +68,59 @@ const ProfilePage = () => {
 
   // 모달 관련 로직
   const modalOpen = () => {
-    setIsModalOpen(true)
-  }
+    setIsModalOpen(true);
+  };
 
   const modalClose = () => {
-    setIsModalOpen(false)
-  }
-
+    setIsModalOpen(false);
+  };
 
   return (
     <div>
-      <Header title="내 프로필 만들기"/>
-      <div className='px-5'>
-        <label htmlFor="nickname" className="block text-sm font-medium leading-6 text-gray-900">닉네임</label>
-        <div className="relative mt-2 rounded-md shadow-sm">
+      <div className="mb-20">
+        <ProfileHeader title="내 프로필 만들기" onConfirmClick={handleUserUpdateClick} />
+      </div>
+      <div className="px-5">
+        <div className="px-5 rounded-md shadow-sm">
           <input
             type="text"
             id="nickname"
             value={newNickname}
             onChange={handleNicknameChange}
-            className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-            placeholder={nickName} />
+            className="block w-full py-1.5 pl-2 pr-20 text-gray-900 placeholder:text-gray-400 sm:text-sm sm:leading-6 border-b-2 border-slate-700"
+            placeholder={nickName}
+          />
+        </div>
+
+        <div onClick={modalOpen} className="mt-24 mb-20 h-full flex flex-col justify-center items-center">
+          <div className="relative">
+            <img src={myCharacter} alt={`${newCharacterId}번 캐릭터`} />
+            <div className="absolute right-0 bottom-0">
+              <img src={editIcon} alt="수정 아이콘" className="w-8 h-auto"></img>
+            </div>
+          </div>
+          <h5>캐릭터 변경하기</h5>
         </div>
 
         <div>
-          <label htmlFor="status" className="block text-sm font-medium leading-6 text-gray-900">상태메시지</label>
-          <div className="relative mt-2 rounded-md shadow-sm">
+          {isModalOpen && (
+            <CharacterModal onClose={modalClose} changeCharacter={handleCharacterChange} nowCharacterId={newCharacterId} />
+          )}
+        </div>
+
+        <div>
+          <div className="relative px-5 rounded-md shadow-sm">
+            <label htmlFor="status" className="mb-2 pl-2 block text-sm font-medium leading-6 text-gray-900">상태메시지</label>
             <input
               type="text"
               id="status"
               value={newStatus}
               onChange={handleStatusChange}
-              className="block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              placeholder={status} />
+              className="block w-full py-1.5 pl-2 pr-20 text-gray-900 placeholder:text-gray-400 sm:text-sm sm:leading-6 border-b-2 border-slate-700"
+              placeholder={status}
+            />
           </div>
-          <div className=' text-gray-500'> 최소 2글자 이상 작성해주세요. </div>
-
-
-          <div
-            onClick={modalOpen}
-            className="flex justify-center items-center" style={{ height: "100" }}>
-            <img src={myCharacter} alt={`${newCharacterId}번 캐릭터`} />
-          </div>
-
-          <div>
-            {isModalOpen && (
-              <CharacterModal onClose={modalClose} changeCharacter={handleCharacterChange}
-                nowCharacterId={newCharacterId} />
-            )}
-          </div>
-          <button onClick={handleUserUpdateClick}>확인</button>
+          <h5 className="mt-2 px-5 text-gray-500">* 최소 2글자 이상 작성해주세요. </h5>
         </div>
       </div>
     </div>

89612d0aa3e3b4fb4ab04c2ef179c754e94e2de9
최동우 <fdf5548@naver.com>
2023-11-02 21:59:28 +0900

    ♻️ Refactor : LandMark querydsl 적용

---
+++ backend/src/main/java/com/example/backend/repository/mariaDB/record/CustomRecordRepository.java
@@ -0,0 +1,33 @@
+package com.example.backend.repository.mariaDB.record;
+
+import static com.example.backend.entity.mariaDB.member.QMember.member;
+import static com.example.backend.entity.mariaDB.space.QRecord.record;
+import static com.example.backend.entity.mariaDB.space.QRecordComment.recordComment;
+
+import com.example.backend.entity.mariaDB.space.Record;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CustomRecordRepository extends QuerydslRepositorySupport{
+
+    private final JPAQueryFactory queryFactory;
+
+    public CustomRecordRepository(JPAQueryFactory queryFactory) {
+        super(Record.class);
+        this.queryFactory = queryFactory;
+    }
+
+    public Optional<Record> findById(Long recordId) {
+        return Optional.ofNullable(queryFactory
+                .selectFrom(record)
+                .leftJoin(record.member, member)
+                .leftJoin(record.comments, recordComment).fetchJoin()
+                .where(record.id.eq(recordId))
+                .fetchOne()
+        );
+    }
+
+}

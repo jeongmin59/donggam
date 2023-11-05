2e6b98950cd22103da599660fde4fa41d145619f
woodong27 <cdw5548@gmail.com>
2023-11-05 19:13:42 +0900

    ✨ Feat : 채팅방 나가기 및 상대 접속유무 판단

--- frontend/src/pages/ChattingPage.jsx
+++ frontend/src/pages/ChattingPage.jsx
@@ -1,6 +1,6 @@
 import Header from "../components/common/Header";
 import React, { useState, useEffect, useRef } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useLocation } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 import { UserSelector } from '../recoil/user/userSelector';
 import axiosInstance from "../api/axiosConfig";
@@ -8,13 +8,17 @@ import Stomp from 'stompjs';
 import SockJS from 'sockjs-client/dist/sockjs';
 
 const ChattingPage = () => {
-  const { roomId, isActive } = useParams();
+  const { roomId } = useParams();
+  const location = useLocation();
+  const { state } = location;
+  const isActive = state ? state.isActive : false ;
   const [chatList, setChatList] = useState([]);
   const [stompClient, SetStompClient] = useState(null);
   const [message, setMessage] = useState('');
   const user = useRecoilValue(UserSelector);
   const senderId = user.memberId;
   const sender = user.nickname;
+  const [isOpponentOnline, setIsOpponentOnline] = useState(false);
 
   const updateChatList = async () => {
     const res = await axiosInstance.get(`/chat/list/${roomId}`)
@@ -34,6 +38,13 @@ const ChattingPage = () => {
         const message = JSON.parse(response.body);
         setChatList(prevChatList => [...prevChatList, message]);
       }); 
+      // 상대방의 접속 유무 판단
+      stompClient.ws.onopen = () => {
+        setIsOpponentOnline(true);
+      };
+      stompClient.ws.onclose = () => {
+        setIsOpponentOnline(false);
+      }
     });
   };
 
@@ -41,6 +52,7 @@ const ChattingPage = () => {
     updateChatList();
     updateStompClient();
     console.log(isActive);
+    console.log('isOpponentOnline : ' + isOpponentOnline);
   }, [])
 
   // 메시지 전송
@@ -51,28 +63,33 @@ const ChattingPage = () => {
       "sender" : sender,
       "senderId" : senderId,
       "content" : message,
+      "isRead" : isOpponentOnline
     };
     stompClient.send(`/pub/chat/message`, {}, JSON.stringify(request));
     setMessage('');
   }
 
   return (
-    <>
       <div className="chatting h-screen bg-gradient-to-b from-[#e5f3ff] to-white">
         <Header title="1:1 채팅방" />
         <ul>
           {chatList.length > 0 && chatList.map(chat => (
-            <li key={chat.id}>{chat.sender} : {chat.content}</li>
+            <li key={chat.id}>{chat.sender} : {chat.content} - 읽음유무 : {chat.isRead ? '읽음' : '안읽음'}</li>
           ))}
         </ul>
-        <input
-          type="text"
-          value={message}
-          onChange={e=>setMessage(e.target.value)}
-        />
-        <button onClick={handleSendMessage}>전송</button>
+        {isActive ? (
+          <>
+            <input
+              type="text"
+              value={message}
+              onChange={e=>setMessage(e.target.value)}
+            />
+            <button onClick={handleSendMessage}>전송</button>
+          </>
+        ) : (
+          <p>상대방이 나가서 비활성화 상태입니다.</p>
+        )}
       </div>
-    </>
   );
 };
 

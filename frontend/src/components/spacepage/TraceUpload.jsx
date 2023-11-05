95441ee797066df5dd0840f2c3937daa8a509fd1
양불회 <yubeak01@naver.com>
2023-11-05 17:23:07 +0900

    :sparkles: Feat: 위도, 경도 저장 atom 생성및 메인페이지 위치api 호출 시 위도, 경도 정보 전역 상태에 저장

--- frontend/src/components/spacepage/TraceUpload.jsx
+++ frontend/src/components/spacepage/TraceUpload.jsx
@@ -5,17 +5,21 @@ import UploadSpaceContent from "./UploadSpaceContent";
 import UploadButton from "../common/UploadButton";
 
 const TraceUpload = () => {
+  // 여기서 업로드 axios 요청을 해야함
+  // 사진, 제목, 내용 중 하나라도 있으면 요청 가능!
+  // 위도, 경도는 전역 상태 관리를 통해서 하면 되지 않을까?
+
 
   return (
     <>
       <div className="mt-3 px-5 pb-10">
         {/* 정보나 함수 넘겨주는 건 아무것도 안돼있어용 ~. .  */}
         <UploadSpacePhoto />
-        <UploadSpaceTitle title="방명록 이름을 지어주세요"/>
+        <UploadSpaceTitle title="방명록 이름을 지어주세요" />
         <UploadSpaceContent />
 
         {/* UploadButton.jsx 참고해서 처리해야 합니닷 */}
-        <UploadButton /> 
+        <UploadButton />
       </div>
     </>
   );

5c080f98091a4dcfba3344bb6bce50549bd59fc4
최동우 <fdf5548@naver.com>
2023-11-05 17:16:50 +0900

    ♻️ Refactor : Socket통신 origin 수정
    Change-Id: If636155f08e198f73933de7354474ae0c3010c78

--- backend/src/main/java/com/example/backend/config/WebSocketConfig.java
+++ backend/src/main/java/com/example/backend/config/WebSocketConfig.java
@@ -26,7 +26,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/stomp/chat")
 //                .setAllowedOriginPatterns("*").withSockJS();
-                .setAllowedOriginPatterns("*");
+                .setAllowedOrigins("*");
     }
 
     @Override

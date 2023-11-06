56f266966d53543fc0995cea5aa2ce12770c4fac
공정민 <jmgong59@gmail.com>
2023-11-06 11:26:09 +0900

    :sparkles: Feat : 쪽지 전송 시 사진 용량 줄임, 쪽지 없을 때 화면 css 완료

--- frontend/src/components/mailboxpage/MailBox.jsx
+++ frontend/src/components/mailboxpage/MailBox.jsx
@@ -4,15 +4,16 @@ import { getMailList, getStatusList } from '../../api/mailApi';
 import { useRecoilValue } from 'recoil';
 import { StatusMessageAtom, StatusMessageIdAtom } from '../../recoil/user/userAtom';
 import StatusList from './StatusList';
+import nullLogo from '../../assets/images/noMail.svg';
 
 const MailBox = () => {
-  const nowStatus = useRecoilValue(StatusMessageAtom)
-  const nowStatusId = useRecoilValue(StatusMessageIdAtom)
+  const nowStatus = useRecoilValue(StatusMessageAtom);
+  const nowStatusId = useRecoilValue(StatusMessageIdAtom);
   const [status, setStatus] = useState(nowStatus);
   const [statusList, setStatusList] = useState([]);
   const [mailList, setMailList] = useState([]);
 
-  const [selectedStatusId, setSelectedStatusId] = useState(nowStatusId)
+  const [selectedStatusId, setSelectedStatusId] = useState(nowStatusId);
 
   const [isModalOpen, setIsModalOpen] = useState(false);
 
@@ -26,39 +27,45 @@ const MailBox = () => {
 
   useEffect(() => {
     getStatusList()
-
       .then((res) => {
-        setStatusList(res.statusList)
-        return getMailList(selectedStatusId)
+        setStatusList(res.statusList);
+        return getMailList(selectedStatusId);
       })
       .then((res) => {
         setMailList(res.data.messageList);
       })
       .catch((err) => {
-        console.log('status리스트 가져오기 실패ㄱ-', err)
-      })
-  }, [selectedStatusId])
-
+        console.log('status리스트 가져오기 실패ㄱ-', err);
+      });
+  }, [selectedStatusId]);
 
   // 선택된 상메 바꿔주는 함수
   const handleStatusChange = ({ selectedStatus, selectedStatusId }) => {
     setStatus(selectedStatus);
-    setSelectedStatusId(selectedStatusId)
+    setSelectedStatusId(selectedStatusId);
   };
 
   return (
-    <div className='px-5'>
+    <div className="px-5">
       <div className="my-8 h-20">
-        <button className="w-full h-full bg-stone-50 rounded-2xl justify-center items-center gap-2.5 inline-flex" onClick={openModal}>{status}</button>
-
+        <button className="w-full h-full bg-stone-50 rounded-2xl justify-center items-center gap-2.5 inline-flex" onClick={openModal}>
+          {status}
+        </button>
         <StatusList isOpen={isModalOpen} onClose={closeModal} statusList={statusList} changeStatus={handleStatusChange} />
       </div>
 
-      <div className='grid grid-cols-3 gap-4'>
-        {mailList.map((mail, index) => (
-          <MailList key={index} mail={mail} />
-        ))}
-
+      <div className="text-center">
+        {mailList.length === 0 ? (
+          <div className="flex flex-col justify-center items-center">
+            <img src={nullLogo} alt="구름" />
+          </div>
+        ) : (
+          <div className='grid grid-cols-3 gap-4'>
+            {mailList.map((mail, index) => (
+              <MailList key={index} mail={mail} />
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
--- frontend/src/components/mainpage/MailModal.jsx
+++ frontend/src/components/mainpage/MailModal.jsx
@@ -14,7 +14,7 @@ const MailModal = ({mailModalInfo, closeMailModal}) => {
   const imageInputRef = useRef(null);
 
   const handleSendMailClick = async () => {
-    if (!content) {
+    if (!content) {                     
       console.log("쪽지 내용을 작성하세요");
       return;
     }
@@ -29,21 +29,40 @@ const MailModal = ({mailModalInfo, closeMailModal}) => {
     }
   }
 
-  // 사진 첨부 
-  const handleImageInputChange = (e) => {
-    if (e.target.files.length >0) {
-      setSelectedImage(e.target.files[0]);
+// 사진 관련
+const handleImageInputChange = async (e) => {
+  const file = e.target.files[0];
+
+  const maxSizeInBytes = 10 * 1024 * 1024; // 10MB
+  const maxWidth = 800;  // 최대 너비
+  const maxHeight = 600; // 최대 높이
+
+  if (file) {
+    try {
+      // 이미지 압축을 위한 설정
+      const options = {
+        maxSizeMB: maxSizeInBytes / (1024 * 1024), // 이미지 크기 제한 (10MB)
+        maxWidthOrHeight: Math.max(maxWidth, maxHeight),
+      };
+
+      // 이미지 압축을 수행하고 압축된 파일을 가져옴
+      const compressedFile = await imageCompression(file, options);
+
+      // 압축된 이미지를 선택된 이미지로 설정
+      setSelectedImage(compressedFile);
       setIsEditingImage(true);
 
-      // 미리보기
-      const file = e.target.files[0];
+      // 압축된 이미지의 미리보기 URL 생성
       const reader = new FileReader();
-      reader.readAsDataURL(file);
+      reader.readAsDataURL(compressedFile);
       reader.onload = () => {
         setImageSrc(reader.result || null);
       };
+    } catch (error) {
+      console.error("이미지 압축 중 에러", error);
     }
-  };
+  }
+};
 
   // console.log(statusId, content, selectedImage)
 

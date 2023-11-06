4c729df1db429394169cab04e2b6f82b8fb33310
박진희 <wlsl0620@naver.com>
2023-11-06 11:07:25 +0900

    :sparkles: Feat: 랜드마크 방명록 작성 구현
    Change-Id: I206b650b3bf3628bd54c4c3e5149c09950528439

--- frontend/src/api/landmarkApi.jsx
+++ frontend/src/api/landmarkApi.jsx
@@ -1,3 +1,4 @@
+import axios from "axios";
 import axiosInstance from "./axiosConfig";
 
 export const searchLandmark = async ( latitude, longitude) => {
@@ -10,6 +11,30 @@ export const searchLandmark = async ( latitude, longitude) => {
     return res.data;
   } catch (err) {
     console.log('랜드마크 검색 axios 실패..', err)
+    return err;
+  }
+};
+
+
+export const postLandmark =async( landmarkId, content, imageFile) => {
+  try {
+    const landmark = {
+      "landmarkId" : landmarkId,
+      "content" : content,
+    }
+    const formData = new FormData()
+    formData.append('request', new Blob([JSON.stringify(landmark)], { type: 'application/json' }));
+    formData.append('image', imageFile)
+
+    const res = await axiosInstance.post(`/space/landmark/${landmarkId}`, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },    
+    });
+    console.log('랜드마크 방명록 작성 axios 성공', res);
+    return res.data;
+  } catch (err) {
+    console.log('랜드마크 방명록 axios 실패',err)
     return err;
   }
 }

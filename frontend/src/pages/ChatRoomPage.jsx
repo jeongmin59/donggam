2a04fad6791b14dd85d520b3848978c3bf35201e
최동우 <fdf5548@naver.com>
2023-11-06 17:58:23 +0900

    🎨 Style : 채팅 목록 페이지 디자인 적용

--- frontend/src/pages/ChatRoomPage.jsx
+++ frontend/src/pages/ChatRoomPage.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Header from './../components/common/Header';
 import axiosInstance from '../api/axiosConfig';
+import { XIcon } from '@heroicons/react/outline';
 
 const ChatRoomPage = () => {
   const navigate = useNavigate();
@@ -33,17 +34,31 @@ const ChatRoomPage = () => {
 
   return (
     <div className="chatting h-screen bg-gradient-to-b from-[#e5f3ff] to-white">
-      <Header title="채팅 목록 보기" to="/"/>
-      <ul>
-        {chatRoom.length > 0 && chatRoom.map(room => (
-          <li key={room.roomId} >
-            <span onClick={() => onClickRoom(room.roomId, room.isActive)}>
-              {room.name}의 채팅방 - 상태 : {room.isActive ? '활성' : '비활성'}
-            </span>
-            <button onClick={() => onClickLeave(room.roomId)}>방나가기</button>
-          </li>
-        ))}
-      </ul>
+      <Header title="채팅 목록 보기" to="/" />
+      <div className="flex flex-wrap gap-4 p-4 md:p-8 lg:p-12">
+        {chatRoom.length > 0 &&
+          chatRoom.map((room) => (
+            <div
+              key={room.roomId}
+              className="w-full md:w-1/2 lg:w-1/3"
+              onClick={() => onClickRoom(room.roomId, room.isActive)}
+            >
+              <div className="bg-white p-6 md:p-8 lg:p-10 flex flex-col justify-between rounded-md shadow-md h-full">
+                <span className="cursor-pointer text-lg md:text-xl lg:text-2xl">
+                  {room.name}
+                </span>
+                <div className="flex justify-end items-center mt-4">
+                  <button
+                    onClick={() => onClickLeave(room.roomId)}
+                    className="text-red-500 hover:text-red-700"
+                  >
+                    <XIcon className="h-5 w-5" />
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
+      </div>
     </div>
   );
 };

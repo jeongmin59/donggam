7f796257914c9f9a35074ffdfeb8203437b45317
최동우 <fdf5548@naver.com>
2023-11-07 09:51:26 +0900

    ✨ Feat : 채팅방 목록 반환값 추가

--- backend/src/main/java/com/example/backend/dto/chat/ChatRoomDto.java
+++ backend/src/main/java/com/example/backend/dto/chat/ChatRoomDto.java
@@ -1,10 +1,14 @@
 package com.example.backend.dto.chat;
 
+import com.example.backend.entity.mariaDB.chat.Chat;
 import com.example.backend.entity.mariaDB.chat.ChatRoom;
 import java.time.LocalDateTime;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Objects;
 import lombok.Builder;
 import lombok.Data;
+import org.w3c.dom.stylesheets.LinkStyle;
 
 @Data
 public class ChatRoomDto {
@@ -17,10 +21,17 @@ public class ChatRoomDto {
         private Integer characterId;
         private Boolean isActive;
         private Integer unReadChatCount;
+        private String lastChat;
         private LocalDateTime lastChatTime;
     }
 
     public static Response toDto(ChatRoom room, Long memberId, Boolean isActive) {
+        List<Chat> chats = room.getChat();
+
+        Chat lastChat = chats.stream().
+                max(Comparator.comparing(Chat::getCreatedAt))
+                .orElse(null);
+
         return Response.builder()
                 .roomId(room.getId())
                 .name(Objects.equals(room.getMember1().getId(), memberId)
@@ -29,7 +40,8 @@ public class ChatRoomDto {
                         ? room.getMember2().getCharacterId() : room.getMember1().getCharacterId())
                 .isActive(isActive)
                 .unReadChatCount((int) room.getChat().stream().filter(chat -> !chat.getIsRead()).count())
-                .lastChatTime(room.getLastChatTime())
+                .lastChatTime(lastChat != null ? lastChat.getCreatedAt() : null)
+                .lastChat(lastChat != null ? lastChat.getContent() : null)
                 .build();
     }
 

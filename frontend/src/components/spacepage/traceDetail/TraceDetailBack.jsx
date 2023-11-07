3fc9b651847931bb832263fdbe42dd8ccead61bc
양불회 <yubeak01@naver.com>
2023-11-07 18:01:23 +0900

    :lipstick: Design: 방명록 댓글창 너비 및 위치 조정
    Change-Id: I28129e7cf7d7fb8c39f2a9c41290943e86881797

--- frontend/src/components/spacepage/traceDetail/TraceDetailBack.jsx
+++ frontend/src/components/spacepage/traceDetail/TraceDetailBack.jsx
@@ -13,14 +13,17 @@ const TraceDetailBack = ({ data, setComment, comment, traceId, setShowFront }) =
   return (
     <div>
       <img src='https://item.kakaocdn.net/do/7277796c3f7ef108040dcfde1bd2fb9c960f4ab09fe6e38bae8c63030c9b37f9' className="trace-image text-justify" />
+
       <TraceDetailTitle title={title} content={location} />
-      <div className='trace-comment'>
+
+      <div className='trace-comment '>
         <div>
           {commentList.map((commentItem, index) => (
             <CommentItem key={index} comment={commentItem} />
           ))}
         </div>
-        <div>
+
+        <div className='flex justify-center'>
           <CommentForm setComment={setComment} comment={comment} traceId={traceId} setShowFront={setShowFront} />
         </div>
       </div>

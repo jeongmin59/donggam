055e15997e2f77d50dc27f3a58381338d0f20d0e
공정민 <jmgong59@gmail.com>
2023-11-07 22:24:31 +0900

    :lipstick: Design : 근처 방명록 화면 css 완료

--- frontend/src/components/spacepage/TraceItem.jsx
+++ frontend/src/components/spacepage/TraceItem.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const TraceItem = ({ title, traceId }) => {
+const TraceItem = ({ title, traceId, index }) => {
+  const isOdd = index % 2 === 1;
+  
+  const itemClasses = `
+    h-12 bg-white rounded-3xl shadow-lg inline-flex justify-center items-center
+    ${isOdd ? 'ml-auto' : 'mr-auto'}  `;
 
   return (
-    <Link to={`/space/trace/${traceId}`} >
-      <div className="w-auto max-w-full h-12 px-2 bg-white rounded-3xl shadow justify-center items-center gap-2.5 inline-flex">
-        <div className="text-center text-blue-200 text-base font-medium font-['Gmarket Sans TTF']">{title}.</div>
-      </div>
-    </Link>
+    <div className={itemClasses}>
+      <Link to={`/space/trace/${traceId}`}>
+        <h4 className="px-10 text-center text-blue-400">{title}</h4>
+      </Link>
+    </div>
   );
 };
 

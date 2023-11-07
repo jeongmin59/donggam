cf80604a6567cc5c12e563f8941f5616b8fa098a
공정민 <jmgong59@gmail.com>
2023-11-07 16:15:38 +0900

    :lipstick: Design : 쪽지 페이지 전체 css 수정, 상메 리스트 css 수정

--- frontend/src/components/mailboxpage/StatusList.jsx
+++ frontend/src/components/mailboxpage/StatusList.jsx
@@ -6,8 +6,8 @@ const StatusList = ({ isOpen, onClose, statusList, changeStatus }) => {
 
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50">
-      <div className="modal-overlay absolute inset-0 bg-black opacity-50" onClick={onClose}></div>
-      <div className="modal-content bg-white p-4 rounded shadow-lg w-96 h-72 overflow-y-auto transform translate-y-1/2 transition-transform duration-300">
+      <div className="modal-overlay absolute inset-0 bg-black opacity-30" onClick={onClose}></div>
+      <div className="modal-content bg-white p-6 rounded-t-2xl shadow-lg w-full h-1/2 overflow-y-auto transform translate-y-1/2">
         <span className="close-button absolute top-2 right-2 text-gray-500 cursor-pointer" onClick={onClose}>X</span>
 
         {statusList.map((status, index) => (

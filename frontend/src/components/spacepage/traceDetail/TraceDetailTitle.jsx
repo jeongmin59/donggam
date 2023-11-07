d052299b69da23654e24d542dfbc334bb685488a
양불회 <yubeak01@naver.com>
2023-11-07 22:44:28 +0900

    :lipstick: Design: 상세페이지 제목 css 수정

--- frontend/src/components/spacepage/traceDetail/TraceDetailTitle.jsx
+++ frontend/src/components/spacepage/traceDetail/TraceDetailTitle.jsx
@@ -5,10 +5,9 @@ const TraceDetailTitle = ({ title, content }) => {
 
   return (
     <div
-      className="trace-title text-center" >
-      {title}
-      <br />
-      {content}
+      className="trace-title text-center content-center" >
+      <h3>{title}</h3>
+      <h5>{content}</h5>
     </div >
   );
 };
--- frontend/src/pages/SpacePage.jsx
+++ frontend/src/pages/SpacePage.jsx
@@ -1,8 +1,4 @@
 import React from "react";
-// import SpaceHeaderTemplate from "../components/spacepage/SpaceHeaderTemplate";
-// import ButtonTemplate from "../components/spacepage/ButtonTemplate";
-// import MyTrace from "../components/spacepage/MyTrace";
-// import Header from "../components/common/Header"
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
@@ -10,7 +6,6 @@ import landmarkImg from "../assets/images/landmark-img.svg"
 import traceImg from "../assets/images/trace-img.svg"
 import myTraceImg from "../assets/images/my-trace-img.svg"
 import SpaceButton from "../components/common/SpaceButton";
-// import createBtn from "../assets/common/createBtn.png";
 import miniLogo from "../assets/tutorial/mini-logo.svg";
 import CreateButton from './../components/common/CreateButton';
 import { useNavigate } from "react-router";
@@ -38,39 +33,36 @@ const SpacePage = () => {
   return (
     <>
       {/* <div className=""> */}
-        <div className="flex justify-center py-10">
-          <img 
-            src={miniLogo}
-            onClick={handleLogoClick}
-          />
-        </div>
+      <div className="flex justify-center py-10">
+        <img
+          src={miniLogo}
+          onClick={handleLogoClick}
+        />
+      </div>
 
-        {/* <div className="px-5 h-[100%] flex-column align-center"> */}
-        <div className="h-full py-[13%] px-5">
-          <Slider {...settings} className="carousel pb-5">
-            <div className="trace-guide ">
-                <div className="flex justify-center"><img src={landmarkImg} alt="" /></div>
-                <SpaceButton title="근처 랜드마크 찾기" to="/space/landmark" />
-            </div>
-            <div className="trace-guide">
-              {/* <div className="flex-column mx-auto"> */}
-                <div className="flex justify-center"><img src={traceImg} alt="" /></div>
-                <SpaceButton title="근처 방명록 찾기" to="/space/trace" />
-              {/* </div> */}
-            </div>
-            <div className="trace-guide">
-              {/* <div className="flex-column mx-auto"> */}
-                <div className="flex justify-center"><img src={myTraceImg} alt="" /></div>
-                <SpaceButton title="내 방명록" />
-              {/* </div> */}
-            </div>
-          </Slider>
-
-        </div>
-
+      {/* <div className="px-5 h-[100%] flex-column align-center"> */}
+      <div className="h-full py-[13%] px-5">
+        <Slider {...settings} className="carousel pb-5">
+          <div className="trace-guide ">
+            <div className="flex justify-center"><img src={landmarkImg} alt="" /></div>
+            <SpaceButton title="근처 랜드마크 찾기" to="/space/landmark" />
+          </div>
+          <div className="trace-guide">
+            {/* <div className="flex-column mx-auto"> */}
+            <div className="flex justify-center"><img src={traceImg} alt="" /></div>
+            <SpaceButton title="근처 방명록 찾기" to="/space/trace" />
+            {/* </div> */}
+          </div>
+          <div className="trace-guide">
+            {/* <div className="flex-column mx-auto"> */}
+            <div className="flex justify-center"><img src={myTraceImg} alt="" /></div>
+            <SpaceButton title="내 방명록" to="/mytrace" />
+            {/* </div> */}
+          </div>
+        </Slider>
+      </div>
       <NavBar />
       <CreateButton to='/space/upload' />
-
     </>
   );
 };

4493300b8729bfcc874801c4052eecce685cdc8d
최동우 <fdf5548@naver.com>
2023-11-08 17:39:38 +0900

    ♻️ Refactor : 서비스워커 등록 수정
    Change-Id: I916edd913c578b2ad969e9d842c03610bc8457c1

--- frontend/src/sw.js
+++ frontend/src/sw.js
@@ -1,14 +1,47 @@
-// install event
-self.addEventListener("install", (e) => {
-  // console.log("[Service Worker] installed");
+// This is the "Offline page" service worker
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+const CACHE = "pwabuilder-page";
+
+// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+const offlineFallbackPage = "ToDo-replace-this-name.html";
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
 });
 
-// activate event
-self.addEventListener("activate", (e) => {
-  // console.log("[Service Worker] actived", e);
+self.addEventListener('install', async (event) => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then((cache) => cache.add(offlineFallbackPage))
+  );
 });
 
-// fetch event
-self.addEventListener("fetch", (e) => {
-  // console.log("[Service Worker] fetched resource " + e.request.url);
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith((async () => {
+      try {
+        const preloadResp = await event.preloadResponse;
+
+        if (preloadResp) {
+          return preloadResp;
+        }
+
+        const networkResp = await fetch(event.request);
+        return networkResp;
+      } catch (error) {
+
+        const cache = await caches.open(CACHE);
+        const cachedResp = await cache.match(offlineFallbackPage);
+        return cachedResp;
+      }
+    })());
+  }
 });

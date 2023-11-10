0fdd2b9bc437e05cf905101956c8842132deee9a
junghyunwoo <wjdgusdndi@gmail.com>
2023-11-10 14:55:31 +0900

    :sparkles: Feat : 채팅 목록 크기 바뀌는 현상 해결

--- frontend/src/pages/ChatRoomPage.jsx
+++ frontend/src/pages/ChatRoomPage.jsx
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import Header from './../components/common/Header';
-import axiosInstance from '../api/axiosConfig';
-import { XIcon } from '@heroicons/react/outline';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import Header from "./../components/common/Header";
+import axiosInstance from "../api/axiosConfig";
+import { XIcon } from "@heroicons/react/outline";
 
 const ChatRoomPage = () => {
   const navigate = useNavigate();
@@ -15,17 +15,17 @@ const ChatRoomPage = () => {
   };
 
   const onClickRoom = (roomId, isActive, roomName) => {
-    navigate(`/chatting/${roomId}`, {state : {isActive, roomName}});
+    navigate(`/chatting/${roomId}`, { state: { isActive, roomName } });
   };
 
   const onClickLeave = async (e, roomId) => {
     e.stopPropagation();
     const res = await axiosInstance.post(`/chat/leave`, {
-      roomId : roomId,
+      roomId: roomId,
     });
     const chatRoomList = res.data.data;
     setChatRoom(chatRoomList);
-  }
+  };
 
   useEffect(() => {
     updateChatRoom();
@@ -34,15 +34,15 @@ const ChatRoomPage = () => {
   return (
     <div className="chatting h-screen bg-gradient-to-b from-[#e5f3ff] to-white">
       <Header title="채팅 목록 보기" to="/" />
-      <div className="flex flex-wrap gap-2 p-4 md:p-6 lg:p-8">
+      <div className="flex flex-wrap gap-2 p-4 w-full">
         {chatRoom.length > 0 &&
           chatRoom.map((room) => (
             <div
               key={room.roomId}
-              className="w-full md:w-1/2 lg:w-1/3 my-2 max-w-screen min-w-[20rem] flex-shrink-0"
+              className="w-full my-2 max-w-screen min-w-[20rem] flex-shrink-0"
               onClick={() => onClickRoom(room.roomId, room.isActive, room.name)}
             >
-              <div className="bg-white p-4 md:p-6 lg:p-8 flex flex-col justify-between rounded-[20px] shadow-md h-full w-full relative">
+              <div className="bg-white p-4 flex flex-col justify-between rounded-[20px] shadow-md h-full w-full relative">
                 <div className="flex items-center w-full">
                   <img
                     src={`/character/${room.characterId}.svg`}
@@ -50,7 +50,7 @@ const ChatRoomPage = () => {
                     className="h-16 w-16 object-contain mr-4"
                   />
                   <div className="flex flex-col flex-grow">
-                    <span className="cursor-pointer text-lg md:text-xl lg:text-2xl mb-2">
+                    <span className="cursor-pointer text-lg mb-2">
                       {room.name}
                     </span>
                     <span className="text-sm text-gray-500">
@@ -62,8 +62,11 @@ const ChatRoomPage = () => {
                       {room.unReadChatCount}
                     </div>
                   )}
-                  <div className="absolute top-0 right-0 p-2 cursor-pointer" onClick={(e) => onClickLeave(e, room.roomId)}>
-                    <XIcon className="h-5 w-5 text-gray-500"/>
+                  <div
+                    className="absolute top-0 right-0 p-2 cursor-pointer"
+                    onClick={(e) => onClickLeave(e, room.roomId)}
+                  >
+                    <XIcon className="h-5 w-5 text-gray-500" />
                   </div>
                 </div>
               </div>

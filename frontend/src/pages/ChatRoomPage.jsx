10714a08ee9a8b60c28192a6224aca3ebb7c9499
박진희 <wlsl0620@naver.com>
2023-11-10 15:49:00 +0900

    :lipstick: Design : 채팅목록 css 수정
    Change-Id: I2ed7b30827baac48e145947271fe6870d74efc4e

--- frontend/src/pages/ChatRoomPage.jsx
+++ frontend/src/pages/ChatRoomPage.jsx
@@ -32,46 +32,50 @@ const ChatRoomPage = () => {
   }, []);
 
   return (
-    <div className="chatting h-screen bg-gradient-to-b from-[#e5f3ff] to-white">
-      <Header title="채팅 목록 보기" to="/" />
-      <div className="flex flex-wrap gap-2 p-4 w-full">
-        {chatRoom.length > 0 &&
-          chatRoom.map((room) => (
-            <div
-              key={room.roomId}
-              className="w-full my-2 max-w-screen min-w-[20rem] flex-shrink-0"
-              onClick={() => onClickRoom(room.roomId, room.isActive, room.name)}
-            >
-              <div className="bg-white p-4 flex flex-col justify-between rounded-[20px] shadow-md h-full w-full relative">
-                <div className="flex items-center w-full">
-                  <img
-                    src={`/character/${room.characterId}.svg`}
-                    alt="대화상대의 캐릭터 이미지"
-                    className="h-16 w-16 object-contain mr-4"
-                  />
-                  <div className="flex flex-col flex-grow">
-                    <span className="cursor-pointer text-lg mb-2">
-                      {room.name}
-                    </span>
-                    <span className="text-sm text-gray-500">
-                      {room.lastChat}
-                    </span>
-                  </div>
-                  {room.unReadChatCount > 0 && (
-                    <div className="flex items-center justify-center h-6 w-6 bg-red-500 rounded-full text-white text-xs">
-                      {room.unReadChatCount}
+    <div>
+      <div className="chatting bg-gradient-to-b from-[#e5f3ff] to-white">
+        <Header title="채팅 목록 보기" to="/" />
+        <div className="flex-col gap-2 p-4 h-[calc(100vh-100px)] overflow-y-auto ">
+          <div className="">
+            {chatRoom.length > 0 &&
+              chatRoom.map((room) => (
+                <div
+                  key={room.roomId}
+                  className="my-2"
+                  onClick={() => onClickRoom(room.roomId, room.isActive, room.name)}
+                >
+                  <div className="bg-white p-4 flex flex-col justify-between rounded-[20px] shadow-md h-full w-full relative ">
+                    <div className="flex items-center w-full ">
+                      <img
+                        src={`/character/${room.characterId}.svg`}
+                        alt="대화상대의 캐릭터 이미지"
+                        className="h-16 w-16 object-contain mr-4"
+                      />
+                      <div className="flex flex-col flex-grow">
+                        <span className="cursor-pointer text-lg mb-2">
+                          {room.name}
+                        </span>
+                        <span className="text-sm text-gray-500">
+                          {room.lastChat}
+                        </span>
+                      </div>
+                      {room.unReadChatCount > 0 && (
+                        <div className="flex items-center justify-center h-6 w-6 bg-red-500 rounded-full text-white text-xs">
+                          {room.unReadChatCount}
+                        </div>
+                      )}
+                      <div
+                        className="absolute top-0 right-0 p-2 cursor-pointer"
+                        onClick={(e) => onClickLeave(e, room.roomId)}
+                      >
+                        <XIcon className="h-5 w-5 text-gray-500" />
+                      </div>
                     </div>
-                  )}
-                  <div
-                    className="absolute top-0 right-0 p-2 cursor-pointer"
-                    onClick={(e) => onClickLeave(e, room.roomId)}
-                  >
-                    <XIcon className="h-5 w-5 text-gray-500" />
                   </div>
                 </div>
-              </div>
-            </div>
-          ))}
+              ))}
+          </div>
+        </div>
       </div>
     </div>
   );

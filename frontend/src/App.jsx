554c11e58277c3a4ff49b789a8430aeadbf729b0
최동우 <fdf5548@naver.com>
2023-11-11 17:08:40 +0900

    ✨ Feat : 서비스워커 오프라인 설정

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,6 +23,17 @@ import LandmarkDetailPage from "./pages/LandmarkDetailPage";
 
 function App() {
 
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+    .register('/sw.js')
+    .then(registration => {
+      console.log("Service Worker registered with scope : ", registration.scope);
+    }) 
+    .catch(error => {
+      console.log("Service Worker registration failed : ", error);
+    })
+  }
+
   const isLoggedIn = useRecoilValue(AccessTokenAtom);
 
   return (

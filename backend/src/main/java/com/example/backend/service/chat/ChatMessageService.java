c58cb9688ac31a0ffabf29e2c9cb664fa04fcc4f
최동우 <fdf5548@naver.com>
2023-11-14 00:12:23 +0900

    ✨ Feat : 채팅 푸시 알림
    Change-Id: I4eea715e6997a6f0b07474f876114788835c97cc

--- backend/src/main/java/com/example/backend/service/chat/ChatMessageService.java
+++ backend/src/main/java/com/example/backend/service/chat/ChatMessageService.java
@@ -6,11 +6,15 @@ import com.example.backend.entity.mariaDB.chat.ChatRoom;
 import com.example.backend.entity.mariaDB.member.Member;
 import com.example.backend.exception.ErrorCode;
 import com.example.backend.exception.type.CustomException;
+import com.example.backend.repository.mariaDB.chat.CustomChatRoomRepository;
 import com.example.backend.repository.mariaDB.member.MemberRepository;
 import com.example.backend.repository.mariaDB.chat.ChatRepository;
 import com.example.backend.repository.mariaDB.chat.ChatRoomRepository;
 import com.example.backend.type.MessageType;
+import com.example.backend.util.fcm.FCMNotificationRequestDto;
+import com.example.backend.util.fcm.FCMNotificationService;
 import java.time.LocalDateTime;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
@@ -22,17 +26,17 @@ import org.springframework.transaction.annotation.Transactional;
 public class ChatMessageService {
 
     private final ChatRoomRepository chatRoomRepository;
+    private final CustomChatRoomRepository customChatRoomRepository;
     private final ChatRepository chatRepository;
     private final MemberRepository memberRepository;
     private final RedisTemplate redisTemplate;
     private final ChannelTopic channelTopic;
+    private final FCMNotificationService fcmNotificationService;
 
     @Transactional
     public SendChatMessageDto.Response sendMessage(SendChatMessageDto.Request request) {
-        ChatRoom chatRoom = chatRoomRepository.findById(request.getRoomId()).get();
-        if (chatRoom == null) {
-            throw new CustomException("존재하지 않는 채팅방입니다.", ErrorCode.NOT_SAME_DATA_VALUE);
-        }
+        ChatRoom chatRoom = customChatRoomRepository.findWithMemberById(request.getRoomId());
+
         Member member = memberRepository.findById(request.getSenderId()).get();
         if (member == null) {
             throw new CustomException("존재하지 않는 회원입니다.", ErrorCode.USER_NOT_FOUND);
@@ -61,6 +65,17 @@ public class ChatMessageService {
             // 그륩 채팅일 경우
             redisTemplate.convertAndSend(topic, request);
         }
+
+        // FCM 알림 설정
+        Long memberId = Objects.equals(chatRoom.getMember1().getId(), request.getSenderId()) ?
+                chatRoom.getMember2().getId() : chatRoom.getMember1().getId();
+        FCMNotificationRequestDto chatAlert = FCMNotificationRequestDto.builder()
+                .memberId(memberId)
+                .title(request.getSender())
+                .body(request.getContent())
+                .build();
+        fcmNotificationService.sendNotificationByToken(chatAlert);
+
         return request.toResponse();
     }
 

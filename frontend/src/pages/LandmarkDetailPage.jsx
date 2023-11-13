38c045e00d3440195202f1f40eb613abd5ebedd7
junghyunwoo <wjdgusdndi@gmail.com>
2023-11-13 17:58:16 +0900

    :sparkles: Feat : 랜드마크 리스트 무한 스크롤

--- frontend/src/pages/LandmarkDetailPage.jsx
+++ frontend/src/pages/LandmarkDetailPage.jsx
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react';
-import { useParams, useLocation, useNavigate } from 'react-router-dom';
-import { getLandmarkGuestbook } from '../api/landmarkApi';
-import LandmarkItem from '../components/landmarkpage/LandmarkItem';
-import Header from '../components/common/Header';
-import CreateButton from '../components/common/CreateButton';
+import React, { useState, useEffect, useRef } from "react";
+import { useParams, useLocation, useNavigate } from "react-router-dom";
+import { getLandmarkGuestbook } from "../api/landmarkApi";
+import LandmarkItem from "../components/landmarkpage/LandmarkItem";
+import Header from "../components/common/Header";
+import CreateButton from "../components/common/CreateButton";
 
 const LandmarkDetailPage = () => {
   const location = useLocation();
@@ -11,14 +11,19 @@ const LandmarkDetailPage = () => {
   const { landmarkId } = useParams();
   const navigate = useNavigate();
   const [landmarkList, setLandmarkList] = useState([]);
+  const [displayData, setDisplayData] = useState([]); // 현재 화면에 보여줄 데이터
+  const [currentPage, setCurrentPage] = useState(0); // 현재 페이지
   const [selectedLandmark, setSelectedLandmark] = useState(landmarkName);
 
+  const itemsPerPage = 5;
+  const containerRef = useRef(null);
+
   const landmarks = [
-    'SSAFY 서울 캠퍼스',
-    'SSAFY 대전 캠퍼스',
-    'SSAFY 광주 캠퍼스',
-    'SSAFY 구미 캠퍼스',
-    'SSAFY 부울경 캠퍼스',
+    "SSAFY 서울 캠퍼스",
+    "SSAFY 대전 캠퍼스",
+    "SSAFY 광주 캠퍼스",
+    "SSAFY 구미 캠퍼스",
+    "SSAFY 부울경 캠퍼스",
   ];
 
   useEffect(() => {
@@ -28,9 +33,36 @@ const LandmarkDetailPage = () => {
         setLandmarkList(res.data);
       })
       .catch((err) => {
-        console.error('랜드마크 방명록 조회 실패', err);
+        console.error("랜드마크 방명록 조회 실패", err);
       });
   }, [landmarkId, landmarkName]);
+
+  // 스크롤 이벤트 핸들러
+  const checkScroll = () => {
+    if (!landmarkList.length) return; // landmarkList 비어있으면 종료
+    const { scrollTop, scrollHeight, clientHeight } = containerRef.current;
+    const isNearBottom = scrollTop + clientHeight >= scrollHeight - 5;
+    if (isNearBottom) {
+      console.log("스크롤이 끝에 도달했습니다!");
+      setCurrentPage((prev) => prev + 1);
+    }
+  };
+
+  useEffect(() => {
+    setDisplayData(landmarkList.slice(0, itemsPerPage * (currentPage + 1)));
+  }, [landmarkList, currentPage]);
+
+  useEffect(() => {
+    //스크롤 이벤트 등록
+    if (containerRef.current) {
+      containerRef.current.addEventListener("scroll", checkScroll);
+    }
+    return () => {
+      if (containerRef.current) {
+        containerRef.current.removeEventListener("scroll", checkScroll);
+      }
+    };
+  }, [checkScroll]);
 
   const handleLandmarkChange = (newLandmark) => {
     setSelectedLandmark(newLandmark);
@@ -44,18 +76,18 @@ const LandmarkDetailPage = () => {
     <>
       <div className="bg-white h-screen">
         <Header title={selectedLandmark} to="/space/" />
-        <div className="px-5 pt-5 overflow-y-auto h-full max-h-[calc(100vh-100px)]">
+        <div
+          className="px-5 pt-5 overflow-y-auto h-full max-h-[calc(100vh-100px)]"
+          ref={containerRef}
+        >
           <div>
-            <div className='flex justify-between'>
-              <label 
-                htmlFor="landmarkSelector"
-                className='text-[14px]'>
-              </label>
+            <div className="flex justify-between">
+              <label htmlFor="landmarkSelector" className="text-[14px]"></label>
               <select
                 id="landmarkSelector"
                 onChange={(e) => handleLandmarkChange(e.target.value)}
                 value={selectedLandmark}
-                className='landmark-selector '
+                className="landmark-selector "
               >
                 {landmarks.map((landmark) => (
                   <option key={landmark} value={landmark}>
@@ -66,7 +98,7 @@ const LandmarkDetailPage = () => {
             </div>
 
             <ul>
-              {landmarkList.map((record, index) => (
+              {displayData.map((record, index) => (
                 <LandmarkItem
                   key={index}
                   time={record.createdAt}
